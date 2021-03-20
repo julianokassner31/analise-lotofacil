4cc843f9af266494c592bca3c33cae3e3f5bbc46
Juliano Kassner <juliano.kassner@ciss.com.br>
2021-03-20 19:21:10 -0300

    Corrigido bug das sequencias

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,123 +1,119 @@
 window.onload = function () {
-	
-	var mapDezenasConcursoToPaintSequencie = buildMapDezenasConcursoToPaintSequencie();
-	
-	var keys = Object.keys(analiseSequencias);
-	var contentSequencias = document.getElementById("content-sequencias");
-	
-	var keysSort = keys.sort((a,b) => {
-	    var aSplit = a.split(";").length;
-	    var bSplit = b.split(";").length;
-	
-	    return aSplit > bSplit ? -1 : aSplit < bSplit ? 1 : 0;
-	});
-	
-	var callbackColorsRandom = function getRandomColor() {
-  		var letters = '0123456789ABCDEF';
-  		var color = '#';
-  		for (var i = 0; i < 6; i++) {
-			color += letters[Math.floor(Math.random() * 16)];
-  		}
-	  		
-		if (color === "#FFFFFF") {
-			
-			return getRandomColor();
-		}
-	
-		return color;
+
+	console.log(analiseSequencias);
+
+	const divContentSequencias = document.getElementById("content-sequencias"),
+		sequenceKeys = getSequenceKeys();
+
+	for (let i = 0; i < sequenceKeys.length; i++) {
+		
+		const divSequencia = createDivSequencias(),
+			sequencie = sequenceKeys[i].split(";"),
+			bgColor = getBgColorRandom(),
+			fontColor = getFontColor(bgColor);
+
+		createDivSeqDezenas(sequencie, divSequencia, bgColor, fontColor);
+
+		const concursos = analiseSequencias[sequenceKeys[i]];
+
+		testePaint(concursos, sequencie, bgColor, fontColor);
+
+		divSequencia.appendChild(createDivQtVezes(sequencie.length, concursos.length));
+
+		createLinksSequenceToConcurso(concursos, divSequencia);
+		
+		divContentSequencias.appendChild(divSequencia);
 	}
+};
 
+function testePaint(concursos, sequencie, bgColor, fontColor) {
 
-	var mapDezenasConcursoToPaintSequencieFilter = new Map();
+	for (let i= 0; i < concursos.length; i++) {
 
-	for (i = 0; i < keysSort.length; i++) {
-		
-		var divSequencia = document.createElement("div");
-		divSequencia.className = "flexWrap";
-		
-		var sequencie = keysSort[i].split(";");
-		
-		var cor = callbackColorsRandom();
-		
-		for (j = 0; j < sequencie.length; j++) {
-			
-			var divSeqDezena = document.createElement("div");
-			divSeqDezena.className = "dezena";
-			divSeqDezena.innerText = sequencie[j];
-			divSeqDezena.style.backgroundColor = cor;
-			divSeqDezena.style.fontWeight = 800;
-			
-			divSequencia.appendChild(divSeqDezena);
+		for (let k = 0; k < sequencie.length; k++) {
+			const dezenaToPaint = document.getElementById(concursos[i] +"_"+sequencie[k]);
+			dezenaToPaint.style.backgroundColor = bgColor;
+			dezenaToPaint.style.color = fontColor;
+			dezenaToPaint.style.fontWeight = '800';
 		}
-		
-		mapDezenasConcursoToPaintSequencie.forEach((dezenas, jogo) => {
-
-			var result = _.intersectionBy(dezenas, sequencie).sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
-			
-			if (_.isEqual(result, sequencie)) {
-
-				mapDezenasConcursoToPaintSequencieFilter.set(jogo, _.difference(dezenas, sequencie));
-
-				for (k = 0; k < sequencie.length; k++) {
-					var dezenaToPaint = document.getElementById(jogo +"_"+sequencie[k]);
-					dezenaToPaint.style.backgroundColor = cor;
-					dezenaToPaint.style.fontWeight = 800;
-				}
-			} else {
-				mapDezenasConcursoToPaintSequencieFilter.set(jogo, dezenas);
-			}
-		});
-			
-		mapDezenasConcursoToPaintSequencie = mapDezenasConcursoToPaintSequencieFilter;
-		
-		var divQtVezes =  document.createElement("div");
-		divQtVezes.style.marginLeft = '10px';
-		divQtVezes.innerText = ' Sequencia com '+ sequencie.length +' dezenas seguidas e saiu '+ analiseSequencias[keysSort[i]].length + ' vezes no(s) ' +
-		' concurso(s) ';
-		
-		var divSpansConcursos = analiseSequencias[keysSort[i]].map(tt => {
-			var span = document.createElement("span");
-			span.style.padding = '0px 3px';
-			var link = document.createElement("a");
-			link.href = "#"+tt;
-			link.innerText = tt;
-			
-			span.appendChild(link);
-			
-			return span;
-		});
-		
-		divSequencia.appendChild(divQtVezes);
-		
-		for (m = 0; m < divSpansConcursos.length; m++) {
-			divSequencia.appendChild(divSpansConcursos[m]);
-		}
-		
-		contentSequencias.appendChild(divSequencia);
 	}
 }
 
-function buildMapDezenasConcursoToPaintSequencie() {
-	var map = new Map();
-	concursos.forEach(c => {
-		map.set(c.idConcurso, [
-			c.prDezena.toString(),
-			c.seDezena.toString(),
-			c.teDezena.toString(),
-			c.qaDezena.toString(),
-			c.qiDezena.toString(),
-			c.sxDezena.toString(),
-			c.stDezena.toString(),
-			c.otDezena.toString(),
-			c.noDezena.toString(),
-			c.dcDezena.toString(),
-			c.dprDezena.toString(),
-			c.dseDezena.toString(),
-			c.dteDezena.toString(),
-			c.dqaDezena.toString(),
-			c.dqiDezena.toString()
-		])
+function getSequenceKeys() {
+	return Object.keys(analiseSequencias).sort((a,b) => {
+		const aSplit = a.split(";").length;
+		const bSplit = b.split(";").length;
+
+		return aSplit > bSplit ? -1 : aSplit < bSplit ? 1 : 0;
 	});
-	
-	return map;
+}
+
+function getFontColor(bgColor) {
+	const nib = bgColor.split(''),
+		r = parseInt(nib[1]+nib[2],16),
+		g = parseInt(nib[3]+nib[4],16),
+		b = parseInt(nib[5]+nib[6],16);
+
+	return ( r * 299 + g * 587 + b * 114) / 1000 > 128 ? '#000000' : '#FFFFFF';
+}
+
+function getBgColorRandom() {
+	const letters = '0123456789ABCDEF';
+	let color = '#';
+
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+
+	return color;
+}
+
+function createLinksSequenceToConcurso(concursos, divSequencia) {
+
+	const divSpansConcursos = concursos.map(concurso => {
+		const span = document.createElement("span");
+		span.style.padding = '0px 3px';
+		const link = document.createElement("a");
+		link.href = "#"+concurso;
+		link.innerText = concurso;
+
+		span.appendChild(link);
+
+		return span;
+	});
+
+	for (let i = 0; i < divSpansConcursos.length; i++) {
+		divSequencia.appendChild(divSpansConcursos[i]);
+	}
+}
+
+function createDivQtVezes(sizeSequencie, numeroVezesSaiu) {
+	const divQtVezes =  document.createElement("div");
+	divQtVezes.style.marginLeft = '10px';
+	divQtVezes.innerText = ' Sequencia com '+ sizeSequencie +' dezenas seguidas e saiu ' + numeroVezesSaiu + ' vezes no(s) ' +
+		' concurso(s) ';
+
+	return divQtVezes;
+}
+
+function createDivSequencias() {
+	const divSequencia = document.createElement("div");
+	divSequencia.className = "flexWrap";
+
+	return divSequencia;
+}
+
+function createDivSeqDezenas(sequencie, divSequencia, bgColor, fontColor) {
+
+	for (let i = 0; i < sequencie.length; i++) {
+
+		const divSeqDezena = document.createElement("div");
+		divSeqDezena.className = "dezena";
+		divSeqDezena.innerText = sequencie[i];
+		divSeqDezena.style.backgroundColor = bgColor;
+		divSeqDezena.style.color = fontColor;
+		divSeqDezena.style.fontWeight = '800';
+
+		divSequencia.appendChild(divSeqDezena);
+	}
 }
